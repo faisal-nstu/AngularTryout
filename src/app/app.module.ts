39760ac306b2aa3a86b09ac344b4206de400e592
faisal-nstu <faisal.nstu@ymail.com>
2017-11-03 19:04:31 +0600

    gethero with http service upto: https://angular.io/tutorial/toh-pt6#get-hero-by-id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-
+import { HttpModule }    from '@angular/http';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
@@ -10,19 +10,25 @@ import { RouterModule }   from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { AppRoutingModule } from './app-routing.module'
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
   imports:      [ 
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
    ],
   declarations: [ 
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent,
+    HeroesComponent
    ],
    providers: [HeroService],
-  bootstrap:    [ AppComponent ]
+   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
